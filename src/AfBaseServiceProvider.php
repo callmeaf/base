c50c534a79cbc955ba52dc0a78cfe206d16e6267
callmeaf <alikhoshkar2000@gmail.com>
2023-11-16 10:35:31 +0330

    change service provider name

--- src/AfBaseServiceProvider.php
+++ src/AfBaseServiceProvider.php
@@ -4,7 +4,7 @@ namespace Callmeaf\Base;
 
 use Illuminate\Support\ServiceProvider;
 
-class CallmeafBaseServiceProvider extends ServiceProvider
+class AfBaseServiceProvider extends ServiceProvider
 {
     public function boot()
     {
