d2bc78c8ed8fce688d62a2657f1a36def3b2a5dc
callmeaf <alikhoshkar2000@gmail.com>
2025-04-21 18:19:35 +0330

    add base pivot model methods

--- src/App/Models/BasePivotModel.php
+++ src/App/Models/BasePivotModel.php
@@ -2,10 +2,11 @@
 
 namespace Callmeaf\Base\App\Models;
 
+use Callmeaf\Base\App\Models\Contracts\BaseConfigurable;
 use Callmeaf\Base\App\Traits\Model\BasePivotModelMethods;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class BasePivotModel extends Pivot
+class BasePivotModel extends Pivot implements BaseConfigurable
 {
     use BasePivotModelMethods;
 }
--- src/App/Traits/Model/BasePivotModelMethods.php
+++ src/App/Traits/Model/BasePivotModelMethods.php
@@ -4,5 +4,10 @@ namespace Callmeaf\Base\App\Traits\Model;
 
 trait BasePivotModelMethods
 {
+    abstract static public function configKey(): string;
 
+    public static function config(): array
+    {
+        return config(static::configKey() . '-' . requestVersion());
+    }
 }
